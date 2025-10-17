5d5a0047beb280c2f117b0fa22415a3af221f97a
revil <revil.com@gmail.com>
2025-10-17 15:32:42 +0900

    refactor: interner locking

--- src/tools/interner.rs
+++ src/tools/interner.rs
@@ -2,43 +2,52 @@ use hashbrown::HashMap as FastHashMap;
 use parking_lot::RwLock;
 
 #[derive(Debug, Default)]
+struct InternerInner {
+    map: FastHashMap<Box<str>, u32>,
+    rev: Vec<Box<str>>,
+}
+
+#[derive(Debug, Default)]
 pub struct Interner {
-    map: RwLock<FastHashMap<Box<str>, u32>>,
-    rev: RwLock<Vec<Box<str>>>,
+    inner: RwLock<InternerInner>,
 }
 
 impl Interner {
     pub fn new() -> Self {
-        Self {
-            map: RwLock::new(FastHashMap::new()),
-            rev: RwLock::new(Vec::new()),
-        }
+        Self::default()
     }
 
     #[inline]
     pub fn intern(&self, s: &str) -> u32 {
-        if let Some(id) = self.map.read().get(s).copied() {
+        if let Some(id) = self
+            .inner
+            .read()
+            .map
+            .get(s)
+            .copied()
+        {
             return id;
         }
 
-        // Upgrade to write: allocate once and record both tables
-        let mut map = self.map.write();
-        if let Some(&id) = map.get(s) {
+        let mut inner = self.inner.write();
+        if let Some(&id) = inner.map.get(s) {
             return id;
         }
-        let mut rev = self.rev.write();
-        let id = rev.len() as u32;
+
+        let id = inner.rev.len() as u32;
         let boxed = s.to_string().into_boxed_str();
-        rev.push(boxed.clone());
-        map.insert(boxed, id);
+        inner.rev.push(boxed.clone());
+        inner.map.insert(boxed, id);
         id
     }
 
     #[inline]
     pub fn runtime_cleanup(&self) {
-        let mut rev = self.rev.write();
+        let mut inner = self.inner.write();
 
-        rev.clear();
-        rev.shrink_to_fit();
+        inner.map.clear();
+        inner.map.shrink_to_fit();
+        inner.rev.clear();
+        inner.rev.shrink_to_fit();
     }
 }
